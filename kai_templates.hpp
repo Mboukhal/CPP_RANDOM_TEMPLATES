6158ccc9b44ea8bac95f7c96c4500427f5ba02dc
Mboukhal <85312140+Mboukhal@users.noreply.github.com>
2023-03-11 00:10:30 +0100

    Create kai_templates.hpp

---
+++ kai_templates.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+// erase if exist
+template <typename T1, typename T2>
+void    tryToErase( T1 container ,T2 element ) {
+
+    typename T1::iterator it;
+
+    it = std::find( container.begin(), container.end(), element );
+    if ( it != container.end() )
+        container.erase( it );
+}
+
+// add non existed elemant
+template <typename T1, typename T2>
+void    tryPushBackNonDuplicate( T1 container ,T2 element ) {
+
+    typename T1::iterator it;
+
+    it = std::find( container.begin(), container.end(), element );
+    if ( it == container.end() )
+        container.push_back( it );
+}
